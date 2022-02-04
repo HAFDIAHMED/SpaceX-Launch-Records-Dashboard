dae4f7c9835ec8942438b7086a6f6ad49e186435
AHMED <ahafdi@gear9.ma>
2022-02-04 10:56:54 +0100

    changes

--- app1.py
+++ app1.py
@@ -3,8 +3,8 @@
 
 import pandas as pd
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
